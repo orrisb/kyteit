c51be8a0457179adccf8ff7ee5e29560a99fd7d1
orrisb <internetearnings@live.com>
2019-07-02 19:00:55 -0400

    Add files via upload

---
+++ mouseovers.js
@@ -0,0 +1,14 @@
+<!-- Hide script from old browers
+if (document.images) {
+
+bullet_menu_on = new Image
+bullet_menu_off = new Image
+bullet_menu_on.src = "images/bullet_menu_on.gif"
+bullet_menu_off.src = "images/bullet_menu_off.gif"
+ 
+}
+else {
+
+}
+// end hiding script from old browsers -->
+
